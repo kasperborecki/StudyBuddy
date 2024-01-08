956be1214c300d475fac3b503a86263df600cb31
Kasper Borecki <kasper.borecki05@gmail.com>
2024-01-08 06:13:26 +0100

    layout

---
+++ src/components/LoginBackground.Component.tsx
@@ -0,0 +1,155 @@
+import {IoSchoolOutline} from 'react-icons/io5';
+import {CiVial} from 'react-icons/ci';
+import {CiCalculator1} from 'react-icons/ci';
+import {BsGlobeAmericas} from 'react-icons/bs';
+import {LuBookMarked} from 'react-icons/lu';
+import {GoPencil} from 'react-icons/go';
+import {RxDesktop} from 'react-icons/rx';
+import {ImLab} from 'react-icons/im';
+import {BiAtom} from 'react-icons/bi';
+import {LuTreePine} from 'react-icons/lu';
+import Background from '../assets/background.png';
+
+
+    // <IoSchoolOutline />
+    // <CiVial />
+    // <CiCalculator1 />
+    // <BsGlobeAmericas />
+    // <LuBookMarked />
+    // <FaFlagUsa />
+    // <GoPencil />
+    // <RxDesktop />
+    // <ImLab />
+    // <BiAtom />
+    // <LuTreePine />
+
+  
+  
+  const LoginBackgroundComponent = () => {
+    return (
+        <>
+        <div className='absolute z-0 w-full'>
+        <img
+          src={Background}
+          alt='Logo'
+          className='w-full'
+        />
+      </div>
+      {/* <table className="absolute table-fixed text-white w-full z-10">
+        <tbody>
+            <tr>
+                <td><IoSchoolOutline className='w-10 h-10 m-2 ml-0'/></td>
+                <td><CiCalculator1 className='w-10 h-10 m-2'/></td>
+                <td><CiVial className='w-10 h-10 m-2'/></td>
+                <td><BsGlobeAmericas className='w-10 h-10 m-2'/></td>
+                <td><LuBookMarked className='w-10 h-10 m-2'/></td>
+                <td><GoPencil className='w-10 h-10 m-2'/></td>
+                <td><ImLab className='w-10 h-10 m-2'/></td>
+                <td><RxDesktop className='w-10 h-10 m-2'/></td>
+                <td><LuTreePine className='w-10 h-10 m-2'/></td>
+                <td><BiAtom className='w-10 h-10 m-2 mr-0'/></td>
+            </tr>
+            <tr>
+                <td><GoPencil className='w-10 h-10 m-2 ml-0'/></td>
+                <td><BsGlobeAmericas className='w-10 h-10 m-2'/></td>
+                <td><LuTreePine className='w-10 h-10 m-2'/></td>
+                <td><CiCalculator1 className='w-10 h-10 m-2'/></td>
+                <td><RxDesktop className='w-10 h-10 m-2'/></td>
+                <td><CiVial className='w-10 h-10 m-2'/></td>
+                <td><IoSchoolOutline className='w-10 h-10 m-2'/></td>
+                <td><LuBookMarked className='w-10 h-10 m-2'/></td>
+                <td><BiAtom className='w-10 h-10 m-2'/></td>
+                <td><ImLab className='w-10 h-10 m-2'/></td>
+            </tr>
+            <tr>
+                <td><CiCalculator1 className='w-10 h-10 m-2 ml-0'/></td>
+                <td><GoPencil className='w-10 h-10 m-2'/></td>
+                <td><LuBookMarked className='w-10 h-10 m-2'/></td>
+                <td><CiVial className='w-10 h-10 m-2'/></td>
+                <td><BsGlobeAmericas className='w-10 h-10 m-2'/></td>
+                <td><BiAtom className='w-10 h-10 m-2'/></td>
+                <td><RxDesktop className='w-10 h-10 m-2'/></td>
+                <td><ImLab className='w-10 h-10 m-2'/></td>
+                <td><IoSchoolOutline className='w-10 h-10 m-2'/></td>
+                <td><LuTreePine className='w-10 h-10 m-2'/></td>
+            </tr>
+            <tr>
+                <td><IoSchoolOutline className='w-10 h-10'/></td>
+                <td><CiCalculator1 className='w-10 h-10'/></td>
+                <td><CiVial className='w-10 h-10'/></td>
+                <td><BsGlobeAmericas className='w-10 h-10'/></td>
+                <td><LuBookMarked className='w-10 h-10'/></td>
+                <td><GoPencil className='w-10 h-10'/></td>
+                <td><ImLab className='w-10 h-10'/></td>
+                <td><RxDesktop className='w-10 h-10'/></td>
+                <td><LuTreePine className='w-10 h-10'/></td>
+                <td><BiAtom className='w-10 h-10'/></td>
+            </tr>
+            <tr>
+                <td><GoPencil className='w-10 h-10'/></td>
+                <td><BsGlobeAmericas className='w-10 h-10'/></td>
+                <td><LuTreePine className='w-10 h-10'/></td>
+                <td><CiCalculator1 className='w-10 h-10'/></td>
+                <td><RxDesktop className='w-10 h-10'/></td>
+                <td><CiVial className='w-10 h-10'/></td>
+                <td><IoSchoolOutline className='w-10 h-10'/></td>
+                <td><LuBookMarked className='w-10 h-10'/></td>
+                <td><BiAtom className='w-10 h-10'/></td>
+                <td><ImLab className='w-10 h-10'/></td>
+            </tr>
+            <tr>
+                <td><CiCalculator1 className='w-10 h-10'/></td>
+                <td><GoPencil className='w-10 h-10'/></td>
+                <td><LuBookMarked className='w-10 h-10'/></td>
+                <td><CiVial className='w-10 h-10'/></td>
+                <td><BsGlobeAmericas className='w-10 h-10'/></td>
+                <td><BiAtom className='w-10 h-10'/></td>
+                <td><RxDesktop className='w-10 h-10'/></td>
+                <td><ImLab className='w-10 h-10'/></td>
+                <td><IoSchoolOutline className='w-10 h-10'/></td>
+                <td><LuTreePine className='w-10 h-10'/></td>
+            </tr>
+            <tr className='opacity-80'>
+                <td><IoSchoolOutline className='w-10 h-10 m-2'/></td>
+                <td><CiCalculator1 className='w-10 h-10 m-2'/></td>
+                <td><CiVial className='w-10 h-10 m-2'/></td>
+                <td><BsGlobeAmericas className='w-10 h-10 m-2'/></td>
+                <td><LuBookMarked className='w-10 h-10 m-2'/></td>
+                <td><GoPencil className='w-10 h-10 m-2'/></td>
+                <td><ImLab className='w-10 h-10 m-2'/></td>
+                <td><RxDesktop className='w-10 h-10 m-2'/></td>
+                <td><LuTreePine className='w-10 h-10 m-2'/></td>
+                <td><BiAtom className='w-10 h-10 m-2'/></td>
+            </tr>
+            <tr>
+                <td><GoPencil className='w-10 h-10'/></td>
+                <td><BsGlobeAmericas className='w-10 h-10'/></td>
+                <td><LuTreePine className='w-10 h-10'/></td>
+                <td><CiCalculator1 className='w-10 h-10'/></td>
+                <td><RxDesktop className='w-10 h-10'/></td>
+                <td><CiVial className='w-10 h-10'/></td>
+                <td><IoSchoolOutline className='w-10 h-10'/></td>
+                <td><LuBookMarked className='w-10 h-10'/></td>
+                <td><BiAtom className='w-10 h-10'/></td>
+                <td><ImLab className='w-10 h-10'/></td>
+            </tr>
+            <tr>
+                <td><CiCalculator1 className='w-10 h-10'/></td>
+                <td><GoPencil className='w-10 h-10'/></td>
+                <td><LuBookMarked className='w-10 h-10'/></td>
+                <td><CiVial className='w-10 h-10'/></td>
+                <td><BsGlobeAmericas className='w-10 h-10'/></td>
+                <td><BiAtom className='w-10 h-10'/></td>
+                <td><RxDesktop className='w-10 h-10'/></td>
+                <td><ImLab className='w-10 h-10'/></td>
+                <td><IoSchoolOutline className='w-10 h-10'/></td>
+                <td><LuTreePine className='w-10 h-10'/></td>
+            </tr>
+        </tbody>
+      </table> */}
+      </>
+    );
+  };
+  
+
+export default LoginBackgroundComponent;
--- src/pages/auth/Login.Page.tsx
+++ src/pages/auth/Login.Page.tsx
@@ -1,16 +1,18 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import {ChangeEvent, FormEvent, useState} from 'react';
 import logo from '../../assets/Logo.png';
-import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
+import {IoMdEye, IoMdEyeOff} from 'react-icons/io';
 import supabase from '../../config/SupabaseClient';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import LoginBackgroundComponent from '../../components/LoginBackground.Component';
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+  const [loginError, setLoginError] = useState<boolean>(false);
 
   const [formData, setFormData] = useState({
     email: '',
-    password: ''
+    password: '',
   });
 
   const handleTogglePasswordVisibility = () => {
@@ -20,20 +22,24 @@ const LoginPage = () => {
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setFormData((prevFormData) => ({
       ...prevFormData,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     }));
-    console.log(formData);
+    setLoginError(false);
   };
 
   const handleSubmit = async (e: FormEvent): Promise<void> => {
     e.preventDefault();
     try {
-      const { error } = await supabase.auth.signInWithPassword({
+      const {error} = await supabase.auth.signInWithPassword({
         email: formData.email,
-        password: formData.password
+        password: formData.password,
       });
-      navigate('/');
-  
+      if (error === null) {
+        navigate('/');
+      } else {
+        setLoginError(true);
+      }
+
       if (error) throw error;
     } catch (error) {
       console.error('Authentication error:', error);
@@ -41,63 +47,103 @@ const LoginPage = () => {
   };
 
   return (
-    <div className='flex flex-col items-center justify-start h-screen bg-gradient-to-tr from-[#D687F3] via-[#F6AA80] to-[#FFDD94]'>
-     <div className='mt-[8%]'>
-        <img
-          src={logo}
-          alt='Logo'
-          className='w-[150px] h-[150px]'
-        />
+    <>
+      <div className='z-0'>
+        <LoginBackgroundComponent />
       </div>
-      <form className='mt-[12%] w-screen flex flex-col items-center font-k2d font-bold' onSubmit={handleSubmit}>
-        <div className='w-[80%]'>
-          <label className='ml-4'>
-            Email:
-          </label>
-          <input
-            className='bg-[#ccabd8] border-2 border-black w-full h-[50px] rounded-3xl mb-[8%] pl-4 focus:outline-none focus:border-purple-500'
-            placeholder='Przykład@gmail.com'
-            name='email'
-            onChange={handleChange}
-          />
-          <label className='ml-4'>
-            Hasło:
-          </label>
-          <div className='relative'>
-            <input
-              type={isPasswordVisible ? 'text' : 'password'}
-              className='bg-[#ccabd8] border-2 border-black w-full h-[50px] rounded-3xl mb-[8%] pl-4 focus:outline-none focus:border-purple-500'
-              placeholder='Przykład123!'
-              name='password'
-              onChange={handleChange}
-            />
-            <button
-              type='button'
-              onClick={handleTogglePasswordVisibility}
-              className='absolute top-3 right-5'>
-              {isPasswordVisible ? (
-                <IoMdEye className='w-6 h-6' />
-              ) : (
-                <IoMdEyeOff className='w-6 h-6' />
-              )}
-            </button>
-          </div>
-          <div className='flex ml-5'>
-            <input
-              type='checkbox'
-              id='rememberMe'
-              className='w-4 h-4 mt-0.5 accent-purple-500'
-            />
-            <label htmlFor='rememberMe' className='ml-3'>
-              Zapamiętaj mnie
-            </label>
-          </div>
+      <div className='absolute flex flex-col items-start justify-start'>
+        <div className='absolute mt-12 ml-8 text-white z-10'>
+          <p className='text-xl'>Witaj Ponownie,</p>
+          <p className='text-4xl'>Zaloguj się!</p>
         </div>
-        <button className='bg-[#ccabd8] border-2 border-black w-[50%] h-[50px] rounded-3xl mt-[8%]' type='submit'>
-          ZALOGUJ SIĘ
-        </button>
-      </form>
-    </div>
+        <form
+          className='mt-72 pt-8 flex flex-col items-center font-k2d font-bold h-[400px] w-full mx-11 z-10 rounded-xl'
+          onSubmit={handleSubmit}>
+          <div className='w-[80%]'>
+            <div className='mb-4'>
+              <div className='relative mb-6'>
+                <input
+                  id='email'
+                  name='email'
+                  type='text'
+                  className={`border w-full h-9 shadow-lg py-1 focus:border-2  pl-2 rounded-lg transition-colors focus:outline-none peer bg-inherit text-black ${
+                    loginError
+                      ? 'border-[#ff0000] focus:border-[#ff0000]'
+                      : 'border-gray-300 focus:border-[#893EFF]'
+                  }`}
+                  onChange={handleChange}
+                />
+                <label
+                  className={`absolute left-0 top-1 cursor-text transition-all px-2 bg-white ${
+                    formData.email
+                      ? loginError
+                        ? '-top-4 text-[#ff0000] ml-4'
+                        : '-top-4 text-[#893EFF] ml-4'
+                      : loginError
+                        ? 'peer-focus:text-xs ml-2 mt-1'
+                        : 'peer-focus:text-xs ml-2 mt-1'
+                  } text-black`}>
+                  Email
+                </label>
+              </div>
+              <div className='relative mb-6'>
+                <input
+                  id='password'
+                  name='password'
+                  type={isPasswordVisible ? 'text' : 'password'}
+                  className={`border w-full h-9 shadow-lg py-1 focus:border-2  pl-2 rounded-lg transition-colors focus:outline-none peer bg-inherit text-black ${
+                    loginError
+                      ? 'border-[#ff0000] focus:border-[#ff0000]'
+                      : 'border-gray-300 focus:border-[#893EFF]'
+                  }`}
+                  onChange={handleChange}
+                />
+                <label
+                  className={`absolute left-0 top-1 cursor-text transition-all px-2 bg-white ${
+                    formData.password
+                      ? loginError
+                        ? '-top-4 text-[#ff0000] ml-4'
+                        : '-top-4 text-[#893EFF] ml-4'
+                      : loginError
+                        ? 'peer-focus:text-xs ml-2 mt-1'
+                        : 'peer-focus:text-xs ml-2 mt-1'
+                  } text-black`}>
+                  Password
+                </label>
+                <p className='absolute mt-4 right-0 text-[10px] decoration-2'>
+                  Zapomniałeś hasła?
+                </p>
+                {loginError ? (
+                  <p className='text-[#ff0000] text-[11px] absolute mt-10'>
+                    {' '}
+                    Ops! Coś poszło nie tak, sprawdź ponownie hasło i e-mail.
+                  </p>
+                ) : null}
+                <button
+                  type='button'
+                  onClick={handleTogglePasswordVisibility}
+                  className='absolute top-1.5 right-1 cursor-pointer'>
+                  {isPasswordVisible ? (
+                    <IoMdEye className='w-6 h-6' />
+                  ) : (
+                    <IoMdEyeOff className='w-6 h-6' />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+          <button
+            className='w-60 bg-[#893EFF] h-[50px] mt-20 my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden
+            transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full
+            before:w-full before:h-full before:bg-gradient-to-r before:from-[#58AD33] before:to-[#58AD33] before:transition-all
+            before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]'
+            type='submit'>
+            ZALOGUJ SIĘ
+          </button>
+        <p onClick={(() => navigate('/registration'))}>Nie masz jeszcze konta?</p>
+        </form>
+      </div>
+    </>
   );
 };
 
