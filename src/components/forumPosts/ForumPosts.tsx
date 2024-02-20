199281c4746b69148d650373a5a950e3ce942902
Kasper Borecki <kasper.borecki05@gmail.com>
2024-02-20 13:32:01 +0100

    ForumLayout

--- src/components/forumPosts/ForumPosts.tsx
+++ src/components/forumPosts/ForumPosts.tsx
@@ -1,4 +1,5 @@
-import { IoHeartOutline } from "react-icons/io5";
+import {FaHeart, FaRegEye} from 'react-icons/fa';
+import {IoChatboxEllipsesSharp, IoHeartOutline} from 'react-icons/io5';
 
 interface CusomForumPorstProps {
   name: string;
@@ -22,41 +23,52 @@ const ForumPosts: React.FC<CusomForumPorstProps> = ({
   likes,
 }) => {
   const CDNURL =
-    "https://kgejrkbokmzmryqkyial.supabase.co/storage/v1/object/public/avatars/";
+    'https://kgejrkbokmzmryqkyial.supabase.co/storage/v1/object/public/avatars/';
 
   return (
-    <div className="relative top-40 bg-[#FEECEB] w-full">
-      <div className=" w-[90%] m-[5%] h-[340px] bg-white ">
-        <div className="relative flex flex-row justify-between h-24 w-full p-2">
-          <img
-            src={CDNURL + profileIcon}
-            className=" rounded-full w-[80px] h-[80px]"
-          />
-          <p className="text-[#212427b0] text-[18px] font-semibold -ml-20">
-            {name}
-          </p>
-          <div className="flex flex-col ">
-            <IoHeartOutline className=" text-red-700 w-7 h-7 absolute right-2 " />
-            <p className=" absolute top-12 right-3 text-[#212427b0] text-[15px] font-semibold">
-              {date.slice(0, 10)}
+    <div className='relative top-40 bg-[#FEECEB] w-full'>
+      <div className=' w-[90%] m-[5%] h-[220px] bg-white rounded-xl shadow-md shadow-bottom'>
+        <div className='relative flex flex-row justify-between h-24 w-full p-2'>
+          <div className='w-[80px] h-[80px] p-[3px] bg-gradient-to-r from-yellow-500 via-pink-500 to-red-500 rounded-full'>
+            <img
+              src={CDNURL + profileIcon}
+              className=' rounded-full w-full h-full'
+            />
+          </div>
+          <div className='flex flex-col'>
+            <p className=' text-[18px] font-semibold text-blue-500 break-words -ml-16'>
+              {tittle.slice(0, 20)}...
             </p>
+            <div className='flex flex-row'>
+              <p className='text-[#212427b0] text-[13px] font-semibold -ml-16 mt-2'>
+                {name}
+              </p>
+              <p className='text-[17px] mx-1 mt-1'>•</p>
+              <p className=' text-[#212427b0] text-[13px] font-semibold '>
+                {date.slice(0, 10)}
+              </p>
+            </div>
+          </div>
+          <div className='flex flex-col'>
+            <IoHeartOutline className=' text-red-700 w-7 h-7 absolute right-2 ' />
           </div>
         </div>
-        <div className="flex flex-col p-4 ">
-          <p className=" text-[18px] font-semibold text-blue-500 break-words">
-            {tittle.slice(0, 30)}
-          </p>
-          <p className="break-words">{description.slice(0, 150)}</p>
-          <hr className=" bg-black"></hr>
+        <div className='flex flex-col px-4 '>
+          <p className='break-words'>{description.slice(0, 90)}...</p>
+          <hr className=' bg-black'></hr>
         </div>
-
-        <div className="flex flex-row justify-between px-[15%]">
-          <p className="pt-2 font-bold flex flex-row">
+        <div className='flex flex-row justify-between px-[20%] text-gray-500 -mt-3'>
+          <p className='pt-2 font-bold flex flex-row text-[16px]'>
+            <FaRegEye className='mt-[3px] mr-1 h-5 w-5' />
             {views}
-            <p className=" font-normal pl-2">wyświetleń</p>
           </p>
-          <p className="font-bold flex flex-row ">
-            {likes} <p className="font-normal pl-2">polubień</p>
+          <p className='font-bold flex flex-row text-[16px]'>
+            <IoChatboxEllipsesSharp className='mt-[3px] mr-1 h-5 w-5' />
+            62 
+          </p>
+          <p className='font-bold flex flex-row text-[16px]'>
+            <FaHeart className='mt-[3px] mr-1 h-5 w-5' />
+            {likes}
           </p>
         </div>
       </div>
--- src/pages/forum/Forum.Page.tsx
+++ src/pages/forum/Forum.Page.tsx
@@ -1,18 +1,20 @@
-import { useRecoilState } from "recoil";
-import { DarkModeAtom } from "../../atoms/DarkMode.Atom";
-import { FaSearch } from "react-icons/fa";
-import { BsFilterRight } from "react-icons/bs";
-import { subjectIdAtom, subjectNameAtom } from "../../atoms/Subject.Atom";
-import ForumPosts from "../../components/forumPosts/ForumPosts";
-import { useEffect, useState } from "react";
-import { Forum } from "../../interfaces/Forum.Interfaces";
-import ForumData from "../../services/common/Forum.Selector";
+import {useRecoilState} from 'recoil';
+import {DarkModeAtom} from '../../atoms/DarkMode.Atom';
+import {FaSearch} from 'react-icons/fa';
+import {BsFilterRight} from 'react-icons/bs';
+import {subjectIdAtom, subjectNameAtom} from '../../atoms/Subject.Atom';
+import ForumPosts from '../../components/forumPosts/ForumPosts';
+import {useEffect, useState} from 'react';
+import {Forum} from '../../interfaces/Forum.Interfaces';
+import ForumData from '../../services/common/Forum.Selector';
 
 const ForumPage = () => {
   const [isDarkMode] = useRecoilState(DarkModeAtom);
   const [selectedSubjectName] = useRecoilState(subjectNameAtom);
   const [selectedSubjectId] = useRecoilState(subjectIdAtom);
   const [forumData, setForumData] = useState<Forum[]>([]);
+  const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false);
+  const [selectedFilter, setSelectedFilter] = useState<number>(1);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -20,7 +22,7 @@ const ForumPage = () => {
         const postsRes = await ForumData.getAllPosts(selectedSubjectId);
         setForumData(postsRes);
       } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error('Error fetching posts:', error);
       }
     };
     console.log(selectedSubjectId);
@@ -30,36 +32,92 @@ const ForumPage = () => {
 
   return (
     <div
-      className={`relative min-h-screen flex flex-col items-center justify-start ${
-        isDarkMode ? "bg-[#212121]" : "bg-[#FEECEB]"
-      }`}
-    >
-      <div>
-        <div className="absolute left-0 bg-[#FEECEB] w-[50%] h-20 p-[25px]">
-          <p className="font-semibold text-[22px] text-[#212427] ">Forum</p>
+      className={`relative h-full flex flex-col items-center justify-start pb-40 ${
+        isDarkMode ? 'bg-[#212121]' : 'bg-[#FEECEB]'
+      }`}>
+      <div className='fixed top-0 left-0 right-0 z-40'>
+        <div className='absolute left-0 bg-[#FEECEB] w-[25%] h-20 p-[25px]'>
+          <p className='font-semibold text-[22px] text-[#212427] '>Forum</p>
         </div>
-        <div className="p-7 absolute flex flex-row justify-end right-0 bg-[#FEECEB] w-[50%] h-20">
-          <FaSearch className="w-6 h-6 mx-2 text-gray-500" />
-          <BsFilterRight className="w-7 h-7 -mt-0.5" />
+        <div className='p-7 absolute flex flex-row justify-end right-0 bg-[#FEECEB] w-[76%] h-20 group'>
+          <div className=''>
+            <FaSearch
+              className='z-10 absolute w-8 h-8 -mt-2 left-[79%] text-gray-500 pt-2'
+              onClick={() => setIsSearchOpen(!isSearchOpen)}
+            />
+            <input
+              type='text'
+              placeholder='Wyszukaj'
+              className={`absolute input input-bordered h-10 bg-[#fff7f7] max-w-xs mr-10 w-[90%] -mt-2 left-0 opacity-0 transition ease-in-out duration-200 ${
+                isSearchOpen
+                  ? 'group-hover:opacity-100'
+                  : 'group-hover:opacity-0'
+              }`}
+            />
+          </div>
         </div>
       </div>
-      <div className="absolute flex justify-center h-20 w-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-600 to-gray-800 bg-gradient-to-rs top-20">
-        <p className="text-white text-[22px] text-semibold p-5">
+      <div className='fixed left-0 right-0 flex z-40 justify-center h-20 w-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-600 to-gray-800 bg-gradient-to-rs top-20'>
+        <p className='text-white text-[22px] text-semibold p-5'>
           {selectedSubjectName}
         </p>
       </div>
-      {forumData.map((data) => (
-        <ForumPosts
-          name={"Hubert Czaplicki"}
-          profileIcon={"421992140_1430648034327163_1645715190455232193_n.jpg"}
-          tittle={data.tittle}
-          description={data.description}
-          date={data.created_at}
-          views={data.views}
-          answers={420}
-          likes={data.likes}
-        />
-      ))}
+      <div className='absolute top-44 z-20 flex flex-row'>
+        <div
+          className={`flex w-20 h-7 pt-1  mx-2 rounded-lg justify-center font-semibold ${
+            selectedFilter === 1
+            ? 'bg-[#92b5ff59] text-[#4879e2]'
+            : 'bg-[#d6d6d67a] text-[#8d8d8de1]'
+          }`}
+          onClick={(() => setSelectedFilter(1))}
+          >
+          <p>Najnowsze</p>
+        </div>
+        <div
+          className={`flex w-20 h-7 pt-1  mx-2 rounded-lg justify-center font-semibold ${
+            selectedFilter === 2
+            ? 'bg-[#92b5ff59] text-[#4879e2]'
+            : 'bg-[#d6d6d67a] text-[#8d8d8de1]'
+          }`}
+          onClick={(() => setSelectedFilter(2))}
+          >
+          <p>Najstarsze</p>
+        </div>
+        <div
+          className={`flex w-20 h-7 pt-1  mx-2 rounded-lg justify-center font-semibold ${
+            selectedFilter === 3
+              ? 'bg-[#92b5ff59] text-[#4879e2]'
+              : 'bg-[#d6d6d67a] text-[#8d8d8de1]'
+          }`}
+          onClick={(() => setSelectedFilter(3))}
+          >
+          <p>Trendujące</p>
+        </div>
+        <div
+          className={`flex w-20 h-7 pt-1 mx-2 rounded-lg justify-center font-semibold ${
+            selectedFilter === 4
+            ? 'bg-[#92b5ff59] text-[#4879e2]'
+            : 'bg-[#d6d6d67a] text-[#8d8d8de1]'
+          }`}
+          onClick={(() => setSelectedFilter(4))}
+          >
+          <p>Popularne</p>
+        </div>
+      </div>
+      <div className='mt-10'>
+        {forumData.map((data) => (
+          <ForumPosts
+            name={'Hubert Czaplicki'}
+            profileIcon={'421992140_1430648034327163_1645715190455232193_n.jpg'}
+            tittle={data.tittle}
+            description={data.description}
+            date={data.created_at}
+            views={data.views}
+            answers={420}
+            likes={data.likes}
+          />
+        ))}
+      </div>
     </div>
   );
 };
